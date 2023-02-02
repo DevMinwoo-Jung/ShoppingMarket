51220f14f8d1d9ba2d9c8832546569c925172482
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-02-02 20:30:00 +0900

    add custom hook but got error..

--- joopang/pages/Components/LoginForm/LoginForm.tsx
+++ joopang/pages/Components/LoginForm/LoginForm.tsx
@@ -1,3 +1,4 @@
+import useInput from '@/utils/customHooks/useInput';
 import Image from 'next/image';
 import React from 'react';
 import { useTranslation } from "react-i18next";
@@ -5,11 +6,22 @@ import { useTranslation } from "react-i18next";
 
 const LoginForm = () => {
   const { t } = useTranslation()
+  const [userId, onChangeUserId] = useInput('')
+  const [password, onChangePassword] = useInput('')
 
   return (
-    <div className='w-96 bg-#f2f2f2 m-auto content-center-center text-center mt-14'>
+    <div className='sm:w-80 md:w-96 lg:w-120 h-auto bg-#fffcfc m-auto content-center-center text-center mt-32 border-r-4'>
       <p className="text-2xl">{t('LOGIN')}</p>
       <Image width={200} height={200} className="m-auto" src="/Joopang_logo.png" alt="" />
+      <div className='block'>
+        <div className='mt-4'>
+          <input type="text" value={userId} onChange={onChangeUserId}/>
+        </div>
+        <div className='mt-4'>
+          <input type="text" value={password} onChange={onChangePassword}/>
+
+        </div>
+      </div>
     </div>
   )
 }
