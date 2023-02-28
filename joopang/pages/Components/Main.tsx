871ad1b0f092a55c5ace6225bb36afa7f4b6a760
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-02-28 20:30:00 +0900

    get products info

---
+++ joopang/pages/Components/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Products from './Products/Products'
+
+const Main = () => {
+  return (
+    <div className='w-full h-full'>
+      <Products/>
+    </div>
+  )
+}
+
+export default Main
--- joopang/pages/Components/Products/Products.tsx
+++ joopang/pages/Components/Products/Products.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+interface IProductsInfo {
+  id: string;
+  name: string;
+  price: string;
+  stock: string;
+  brand: string;
+  updateDate: string;
+  src: string;
+}
 
 const Products = () => {
+
+  const [ProductsInfo, setProductsInfo] = useState([])
+
+  useEffect(() => {
+    fetch('/mock/products.json')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('------get Data-----');
+      setProductsInfo(data);
+    })
+    .catch(() => {
+    })
+    return () => {
+      console.log('-------Unmount------');
+    };
+  }, [])
+
   return (
-    <div className='w4/5 h4/5 m-auto top-8 border'>
-      Products
+    <div className='w-3/4 h-3/4 m-auto top-14 border'>
+      {
+        ProductsInfo.map((element:IProductsInfo) => {
+          return (
+            <>
+              {
+                <>
+                  <p>{element.id}</p>
+                  <p>{element.name}</p>
+                  <p>{element.price}</p>
+                  <p>{element.stock}</p>
+                  <p>{element.brand}</p>
+                  <p>{element.updateDate}</p>
+                  <p>{element.src}</p>
+                </>
+              }
+            </>
+          )
+        })
+      }
     </div>
   )
 }
